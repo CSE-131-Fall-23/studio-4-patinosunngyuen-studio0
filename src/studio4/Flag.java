c1396dd72db7eeb018f42905ee33c3b0f817a143
isaiah.patino <isaiah.patino@localhost>
2023-10-04 14:14:36 -0500

    Im done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,31 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		Color mine = new Color(10, 200, 150);
+		StdDraw.setPenColor(mine);
+		StdDraw.filledRectangle(0.5, 0.4, 0.5, 0.45);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(0.5, 0.39, 0.49, 0.45);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledCircle(0.5, 0.6, 0.1);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledCircle(0.3, 0.35, 0.1);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledCircle(0.7, 0.35, 0.1);
+		
+		StdDraw.setPenColor(Color.RED);
+		double[] x = {0.7, 0.4, 0.5};
+		double[] y = {0.1, 0.8, 0.7};
+		StdDraw.filledPolygon(x,y);
+		
+		
+		
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,53 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shape = in.next();
+		System.out.println(shape);
+		
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		Color c = new Color(red, green, blue);
+		
+		Boolean filled = in.nextBoolean();
+		double p1 = in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
+		
+		StdDraw.setPenColor(c);
+		//StdDraw.filledRectangle(p1, p2, p3, p4);
+		StdDraw.rectangle(p1, p2, p3, p4);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	private static Object nextLine() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
